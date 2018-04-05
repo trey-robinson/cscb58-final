d350db1aeaa09bf7b9903f07fc2c1fb9afa3d8bd
ohmanitsbodan <37917227+ohmanitsbodan@users.noreply.github.com>
2018-04-05 16:37:56 -0400

    someone save us

---
+++ backup.v
@@ -0,0 +1,51 @@
+module backup(CLOCK_50,
+						KEY,
+						VGA_CLK,   						//	VGA Clock
+						VGA_HS,							//	VGA H_SYNC
+						VGA_VS,							//	VGA V_SYNC
+						VGA_BLANK_N,						//	VGA BLANK
+						VGA_SYNC_N,						//	VGA SYNC
+						VGA_R,   						//	VGA Red[9:0]
+						VGA_G,	 						//	VGA Green[9:0]
+						VGA_B   						//	VGA Blue[9:0]
+						);
+
+	input			CLOCK_50;				//	50 MHz
+	input   [1:0]   KEY;
+	
+	// Declare your inputs and outputs here
+	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(1'b1),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(1'b1),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+	
+endmodule
